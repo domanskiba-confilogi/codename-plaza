63991011b4e1be4d81e4ca840b6c52653e5b532d
Bartłomiej Domański <domanski.bartlomiej@confilogi.com>
2025-08-15 06:21:06 +0200

    wip

---
+++ migrations/20250813103000_seed_company_departments.sql
@@ -0,0 +1,18 @@
+INSERT INTO company_departments (
+	id,
+	name
+) VALUES
+	(1, 'Call center'),
+	(2, 'IT'),
+	(3, 'Logistics'),
+	(4, 'Warehouse'),
+	(5, 'Personnel administration'),
+	(6, 'HR'),
+	(7, 'Accounting'),
+	(8, 'Legal'),
+	(9, 'Marketing'),
+	(10, 'Analitycs'),
+	(11, 'Core management'),
+	(12, 'Sales'),
+	(13, 'Call review'),
+	(14, 'Controlling');
