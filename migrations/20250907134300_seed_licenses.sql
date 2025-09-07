4ba04fd92aae35245acc5f2822bc4cb756580760
Bartłomiej Domański <domanski.bartlomiej@confilogi.com>
2025-09-07 15:51:42 +0200

    feat(migrations): add seeding for licenses and license mappings to job titles mappings

---
+++ migrations/20250907134300_seed_licenses.sql
@@ -0,0 +1,7 @@
+INSERT INTO licenses (id, name) 
+VALUES 
+	(1, 'No license'),
+	(2, 'Office on web'),
+	(3, 'Office on desktop'),
+	(4, 'PowerBI Pro'),
+	(5, 'PowerBI Premium');
---
+++ migrations/20250907134417_seed_job_titles_have_strict_onboarding_license_mappings.sql
@@ -0,0 +1,8 @@
+INSERT INTO job_titles_have_strict_onboarding_license_mappings (job_title_id, license_id) 
+VALUES 
+	(2, 2),
+	(3, 2),
+	(4, 2),
+	(5, 2),
+	(6, 2),
+	(7, 2);
