269a7914c4b0cd223ce0015a08a0110196fa0365
Bartłomiej Domański <domanski.bartlomiej@confilogi.com>
2025-09-08 13:08:20 +0200

    feat(migrations): add required tables for job title to features mappings

--- migrations/20250905114257_create_job_titles_have_strict_onboarding_system_permissions_mappings_table.sql
+++ migrations/20250905114257_create_job_titles_have_strict_onboarding_system_permissions_mappings_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE job_titles_have_strict_onboarding_system_permissions_table (
+CREATE TABLE job_titles_have_strict_onboarding_system_permissions_mappings (
 	job_title_id INTEGER NOT NULL REFERENCES job_titles (id),
 	system_permission_id INTEGER NOT NULL REFERENCES system_permissions (id),
 
---
+++ migrations/20250908103755_seed_job_titles_have_strict_onboarding_system_permissions_mappings_table.sql
@@ -0,0 +1,10 @@
+INSERT INTO job_titles_have_strict_onboarding_system_permissions_mappings (
+	job_title_id,
+	system_permission_id
+) VALUES 
+	(2, 1), -- Agent PL - Vici PL
+	(3, 2), -- Agent CZ - Vici CZ
+	(4, 3), -- Agent SK - Vici SK
+	(5, 6), -- Agent HR - Vici HR
+	(6, 4), -- Agent SI - Vici SI
+	(7, 5); -- Agent BG - Vici BG
