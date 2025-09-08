26db77d47a2da09f51574a197ce1dc41f0580c4f
Bartłomiej Domański <domanski.bartlomiej@confilogi.com>
2025-09-08 10:09:57 +0200

    feat(migrations): add proper licenses to new job divisions

--- migrations/20250907134417_seed_job_titles_have_strict_onboarding_license_mappings.sql
+++ migrations/20250907134417_seed_job_titles_have_strict_onboarding_license_mappings.sql
@@ -1,8 +1,11 @@
 INSERT INTO job_titles_have_strict_onboarding_license_mappings (job_title_id, license_id) 
 VALUES 
-	(2, 2),
-	(3, 2),
-	(4, 2),
-	(5, 2),
-	(6, 2),
-	(7, 2);
+	(82, 2),
+	(83, 2),
+	(84, 2),
+	(85, 2),
+	(86, 2),
+	(87, 2),
+	(88, 2),
+	(89, 2),
+	(90, 2);
