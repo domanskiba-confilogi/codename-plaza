3f4d33169efd2e88e060c6651ef9afec04267da0
Bartłomiej Domański <domanski.bartlomiej@confilogi.com>
2025-09-08 10:04:41 +0200

    feat(migrations): add system permissions seeding

---
+++ migrations/20250907140459_seed_system_permissions.sql
@@ -0,0 +1,75 @@
+INSERT INTO system_permissions
+	(id, name, subpermission_of_id)
+VALUES
+	(1, 'Vici PL', NULL),
+	(2, 'Vici CZ', NULL),
+	(3, 'Vici SK', NULL),
+	(4, 'Vici SI', NULL),
+	(5, 'Vici BG', NULL),
+	(6, 'Vici HR', NULL),
+	(7, 'Vici IT', NULL),
+
+	(8, 'CRM PL', NULL),
+	(9, 'CRM CZ', NULL),
+	(10, 'CRM SK', NULL),
+	(11, 'CRM SI', NULL),
+	(12, 'CRM BG', NULL),
+	(13, 'CRM HR', NULL),
+	(14, 'CRM IT', NULL),
+
+	(15, 'Optima', NULL),
+
+	(16, 'Manager wallboard PL', 1),
+	(17, 'Manager wallboard CZ', 2),
+	(18, 'Manager wallboard SK', 3),
+	(19, 'Manager wallboard BG', 4),
+	(20, 'Manager wallboard SI', 5),
+
+	(21, 'Access to new panel for Logistics', 8),
+	(22, 'Access to reports tab inside new panel for Logistics', 8),
+	(23, 'Access to complaints tab inside new panel for Logistics', 8),
+	(24, 'Access to shipments tab inside new panel for Logistics', 8),
+	(25, 'Access to imports tab inside new panel for Logistics', 8),
+
+	(26, 'Access to new panel for Logistics', 9),
+	(27, 'Access to reports tab inside new panel for Logistics', 9),
+	(28, 'Access to complaints tab inside new panel for Logistics', 9),
+	(29, 'Access to shipments tab inside new panel for Logistics', 9),
+	(30, 'Access to imports tab inside new panel for Logistics', 9),
+
+	(31, 'Access to new panel for Logistics', 10),
+	(32, 'Access to reports tab inside new panel for Logistics', 10),
+	(33, 'Access to complaints tab inside new panel for Logistics', 10),
+	(34, 'Access to shipments tab inside new panel for Logistics', 10),
+	(35, 'Access to imports tab inside new panel for Logistics', 10),
+
+	(36, 'Access to new panel for Logistics', 11),
+	(37, 'Access to reports tab inside new panel for Logistics', 11),
+	(38, 'Access to complaints tab inside new panel for Logistics', 11),
+	(39, 'Access to shipments tab inside new panel for Logistics', 11),
+	(40, 'Access to imports tab inside new panel for Logistics', 11),
+
+	(41, 'Access to new panel for Logistics', 12),
+	(42, 'Access to reports tab inside new panel for Logistics', 12),
+	(43, 'Access to complaints tab inside new panel for Logistics', 12),
+	(44, 'Access to shipments tab inside new panel for Logistics', 12),
+	(45, 'Access to imports tab inside new panel for Logistics', 12),
+
+	(46, 'Access to sales tab', 8),
+	(47, 'Access to marketing tab', 8),
+
+	(48, 'Access to sales tab', 9),
+	(49, 'Access to marketing tab', 9),
+
+	(50, 'Access to sales tab', 10),
+	(51, 'Access to marketing tab', 10),
+
+	(52, 'Access to sales tab', 11),
+	(53, 'Access to marketing tab', 11),
+
+	(54, 'Access to sales tab', 12),
+	(55, 'Access to marketing tab', 12),
+
+	(56, 'Database replica of Vici PL', NULL),
+	(57, 'Database replica of Vici CZ/SK/SI/BG', NULL),
+	(58, 'Database replica of Vici HR', NULL);
