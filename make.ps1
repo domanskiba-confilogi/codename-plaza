4d5025846412922a6b1aaf9f85d330a04bc9a279
Bartłomiej Domański <domanski.bartlomiej@confilogi.com>
2025-09-09 21:42:20 +0200

    wip

--- make.ps1
+++ make.ps1
@@ -4,7 +4,7 @@ param(
 
 $ErrorActionPreference = "Stop";
 
-$FrontEndPath = $PSScriptRoot + "\\crates\\plaza-client";
+$FrontEndPath = $PSScriptRoot + "\\crates\\pure-plaza-client";
 $BackEndPath = $PSScriptRoot;
 
 $env:DATABASE_URL = 'postgresql://postgres:Confilogi89@127.0.0.1:5432/plaza';
@@ -12,15 +12,12 @@ $env:DATABASE_URL = 'postgresql://postgres:Confilogi89@127.0.0.1:5432/plaza';
 if ($Command -eq "watch-backend") {
 	Set-Location $BackEndPath;
 	cargo watch -x "run -- --db-host 127.0.0.1 --db-port 5432 --db-username postgres --db-password Confilogi89 --db-database plaza";
-} elseif ($Command -eq "watch-frontend") {
-	Set-Location $FrontEndPath;
-	trunk serve;
 } elseif ($Command -eq "watch-frontend-css") {
 	Set-Location $FrontEndPath;
-	npx @tailwindcss/cli -i .\index.css -o .\assets\output.css --watch
+	npx @tailwindcss/cli -i .\tailwind-input.css -o .\tailwind.css --watch
 } elseif ($Command -eq "reset-database") {
 	Set-Location $BackEndPath;
 	cargo sqlx database reset;
 } else {
-	Write-Host "Available commands: 'watch-backend', 'watch-frontend', 'watch-frontend-css', 'reset-database'"
+	Write-Host "Available commands: 'watch-backend', 'watch-frontend-css', 'reset-database'"
 }
