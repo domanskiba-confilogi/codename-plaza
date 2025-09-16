fabe81e5a585b06b7d7f2b51b1f9e775412ef464
Bartłomiej Domański <domanski.bartlomiej@confilogi.com>
2025-09-16 17:02:26 +0200

    .

--- src/ms_graph.rs
+++ src/ms_graph.rs
@@ -32,7 +32,9 @@ impl MsGraphClient {
         url.as_str().to_string()
     }
 
-    pub async fn request_access_token(&self, code: &str) {
+    pub async fn request_access_token(&self, code: &str) -> Result<RequestAccessTokenResponse, RequestAccessTokenError> {
+        type E = RequestAccessTokenError;
+
         let client = reqwest::Client::new();
 
         let form = reqwest::multipart::Form::new()
@@ -43,11 +45,76 @@ impl MsGraphClient {
             .text("grant_type", "authorization_code")
             .text("client_secret", &self.client_secret);
 
-        client.post(&format!("/{tenant_id}/oauth2/v2.0/token"))
+        let response = client.post(&format!("/{tenant_id}/oauth2/v2.0/token"))
             .header("content-type", "application/x-www-form-urlencoded")
             .body(form)
+            .send()
             .await?;
+
+        let status_code = response.status();
+        let response_body = response.text().await;
+
+        if response.status() !== 200 {
+            return Err(E::InvalidStatus(status_code));
+        }
+
+        let response_body = response.json().await
+            .map_err(|error| E::FailedToParseJsonBody(status_code, response_body))?;
+
+        Ok(response_body)
+    }
+
+    pub async fn renew_access_token(&self) -> Result<RenewAccessTokenResponse, RenewAccessTokenError> {
+        type E = RenewAccessTokenError;
+
+        let client = reqwest::Client::new();
+
+        let form = reqwest::multipart::Form::new()
+            .text("client_id", &self.client_id)
+            .text("scope", "User.Read")
+            .text("refresh_token", self.refresh_token)
+            .text("grant_type", "refresh_token")
+            .text("client_secret", &self.client_secret);
+
+        let response = client.post(&format!("/{tenant_id}/oauth2/v2.0/token"))
+            .header("content-type", "application/x-www-form-urlencoded")
+            .body(form)
+            .send()
+            .await?;
+
+        let status_code = response.status();
+        let response_body = response.text().await;
+
+        if response.status() !== 200 {
+            return Err(E::InvalidStatus(status_code));
+        }
+
+        let response_body = response.json().await
+            .map_err(|error| E::FailedToParseJsonBody(status_code, response_body))?;
+
+        Ok(response_body)
     }
 }
 
+#[derive(Debug)]
+enum RenewAccessTokenError {
+    InvalidStatus(u16, Result<String, serde_json::Error>),
+    FailedToParseJsonBody(u16, Result<String, serde_json::Error>)
+}
+
+#[derive(Debug)]
+enum RequestAccessTokenError {
+    InvalidStatus(u16, Result<String, serde_json::Error>),
+    FailedToParseJsonBody(u16, Result<String, serde_json::Error>)
+}
+
 struct FailedToConstructLoginCallbackUrl(url::ParseError);
+
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+pub struct RequestAccessTokenResponse {
+    token_type: String,
+    scope: String,
+    expires_in: u32,
+    access_token: u32,
+    refresh_token: String,
+}
